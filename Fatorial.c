985d08f7d8aa8c9b2a7e9e4273a43a1dc1d2c255
Reueldev <72132440+Reueldev@users.noreply.github.com>
2020-09-30 06:22:40 -0400

    Add files via upload

---
+++ Fatorial.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+	int fatorial = 0;
+    int i = 0;
+    
+    printf ("Digite um numero: ");
+    scanf ("%d", &fatorial);
+    
+    if (fatorial == 0 || fatorial == 1) {
+    	printf ("%d! = %d", fatorial, fatorial);
+	}
+	else if (fatorial > 0) {
+		printf ("\nFatorial de %d: %d! = %d * ", fatorial, fatorial, fatorial);
+		for (i = fatorial - 1; i != 0; i--) {
+			fatorial = fatorial * i;
+			if (i == 1) {
+				printf ("%d ", i);
+			} else {
+				printf ("%d * ", i);
+			}
+		}
+	printf ("= %d\n", fatorial);
+	}
+	else {
+		printf ("\nNumero invalido!");
+	}
+	
+    return (0);
+}
